a9b7150869664fb19b92cdf6201dd3e55f346658
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-03 03:56:18 +0300

    Initial commit

---
+++ HW3.10.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << std::endl;
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+
+	std::cout << "Здравствуйте, " << name << "!";
+	std::cout << std::endl;
+}
